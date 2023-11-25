d210c407fdb916210c3538c848f287a0e5d4131e
Ruslan Zhytkovich <rzhytkovich@gmail.com>
2023-11-25 21:18:12 +0300

    - Included .env.bot - Added welcome message after /start

--- telegram_bot/src/main.py
+++ telegram_bot/src/main.py
@@ -1,22 +1,43 @@
 import asyncio
 import logging
+import os
+import sys
 
 from aiogram import Bot, Dispatcher, types
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from aiogram.utils.markdown import hbold
+from dotenv import load_dotenv
 
+load_dotenv("../.env.bot")
 
-BOT_TOKEN = "6316810679:AAHvpaemNAPnLex1E5uDxoGvIp-BWDD5wn4"
-
-bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
+token = os.getenv("TOKEN")
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    await message.answer(f"Привет, {hbold(message.from_user.full_name)}! Я буду помогать с майнингом!")
 
 
 @dp.message()
-async def echo_message(message: types.Message):
-    await message.answer(text=message.text)
+async def echo_handler(message: types.Message) -> None:
+    try:
+        # Send a copy of the received message
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        # But not all the types is supported to be copied so need to handle it
+        await message.answer("Nice try!")
 
-async def main():
-    logging.basicConfig(level=logging.INFO)
+
+async def main() -> None:
+    # Initialize Bot instance with a default parse mode which will be passed to all API calls
+    bot = Bot(token, parse_mode=ParseMode.HTML)
+    # And the run events dispatching
     await dp.start_polling(bot)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
